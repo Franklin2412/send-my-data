b3b241d8b8d00dc0c56750a1bc5bd934f8da764b
Franklin <franklin.michael@payu.in>
2015-07-12 09:46:08 +0530

    adding postgre

--- app/controllers/receiver_controller.rb
+++ app/controllers/receiver_controller.rb
@@ -3,10 +3,12 @@ class ReceiverController < ApplicationController
   skip_before_filter :verify_authenticity_token, only: [:success, :failure]
 
   def success
+    # render text: "Just returning the post params #{params}"
     render js: "PayU.onSuccess(#{params})"
   end
 
   def failure
+    render text: "Just returning the post params #{params}"
     render js: "PayU.onFailure(#{params})"
   end
 end
